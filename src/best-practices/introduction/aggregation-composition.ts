50c2227e9b3d9c375da69bf31c63c0382e236499
Miguel Luna García <miguel00123456@gmail.com>
2023-03-01 17:54:38 +0000

    Aggregation and composition example

---
+++ src/best-practices/introduction/aggregation-composition.ts
@@ -0,0 +1,33 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Adriano dos Santos Moreira
+ * @author Miguel Luna García
+ * @since Feb 27 2023
+ * @desc Class aggregation and composition example.
+ */
+
+class AClass {
+  sayHi(): void {
+    console.log('Hi!');
+  }
+}
+
+// Aggregation
+class BClass {
+  constructor(private aClass: AClass) {}
+  sayHi(): void {
+    this.aClass.sayHi();
+  }
+}
+
+// Composition
+class CClass {
+  sayHi(): void {
+    this.aClass.sayHi();
+  }
+  private aClass = new AClass();
+}
