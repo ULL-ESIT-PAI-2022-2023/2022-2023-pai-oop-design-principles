5a9d38c1e8ebac89888463a3240d3afc048255f6
Miguel Luna García <miguel00123456@gmail.com>
2023-03-01 17:53:39 +0000

    Object example

---
+++ src/best-practices/introduction/object.ts
@@ -0,0 +1,22 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Adriano dos Santos Moreira
+ * @author Miguel Luna García
+ * @since Feb 27 2023
+ * @desc Object declaration example.
+ */
+
+let employee: object;
+
+employee = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 25,
+  jobTitle: 'Web Developer'
+};
+
+console.log(employee);
