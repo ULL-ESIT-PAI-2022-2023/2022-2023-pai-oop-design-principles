52177df12a23f15945b52f1f66e3675bb62b8fb8
AdrianoMoreira08 <alu0101436784@ull.edu.es>
2023-03-02 10:27:37 +0000

    Shotgun surgery example

---
+++ src/design-principles/code-smells/shotgun.ts
@@ -0,0 +1,25 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Adriano dos Santos Moreira
+ * @author Miguel Luna García
+ * @since Feb 27 2023
+ * @desc Shotgun Surgery example.
+ * If you change the logging behavour, you have to change it in all the functions.
+ */
+
+function myFunction() {
+  console.log("Entering myFunction");
+  /* ... */
+}
+function myFunction2() {
+  console.log("Entering myFunction2");
+  /* ... */
+}
+function myFunction3() {
+  console.log("Entering myFunction3");
+  /* ... */
+}
