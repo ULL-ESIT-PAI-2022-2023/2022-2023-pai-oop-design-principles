4c5baad876126de5ff2a0402975b59edbc532aad
AdrianoMoreira08 <alu0101436784@ull.edu.es>
2023-03-02 10:18:05 +0000

    Code smells examples

---
+++ src/design-principles/code-smells/dupe.ts
@@ -0,0 +1,21 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Adriano dos Santos Moreira
+ * @author Miguel Luna García
+ * @since Feb 27 2023
+ * @desc Duplicated code example.
+ */
+
+function addOne(input: number) {
+  return input + 1;
+}
+function addTwo(input: number) {
+  return input + 2;
+}
+function addThree(input: number) {
+  return input + 3;
+}
---
+++ src/design-principles/code-smells/names.ts
@@ -0,0 +1,18 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Adriano dos Santos Moreira
+ * @author Miguel Luna García
+ * @since Feb 27 2023
+ * @desc Mysterious Names example.
+ */
+
+class DtaRcrd102 {
+  private genymdhms: number = 2;
+  private modymdhms: number = 3;
+  private pszqint: string = "102";
+  /* ... */
+};
