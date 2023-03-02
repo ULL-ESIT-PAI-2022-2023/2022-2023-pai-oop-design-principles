9cffb789c5d91290cdd6b5b095110ef84588fcf0
AdrianoMoreira08 <alu0101436784@ull.edu.es>
2023-03-02 10:58:44 +0000

    Lazy class example

---
+++ src/design-principles/code-smells/lazy-class.ts
@@ -0,0 +1,15 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Adriano dos Santos Moreira
+ * @author Miguel Luna García
+ * @since Feb 27 2023
+ * @desc Lazy class example.
+ */
+
+class MyNumber {
+  constructor(public number: number) {}
+}
