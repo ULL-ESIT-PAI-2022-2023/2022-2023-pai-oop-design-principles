1a2b884c4aa61ad304339b7f7fe7a8d4c02265f6
AdrianoMoreira08 <alu0101436784@ull.edu.es>
2023-03-05 20:42:08 +0000

    Added deeply nested statements example

---
+++ src/design-principles/code-smells/deeply-nested-statements.ts
@@ -0,0 +1,29 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Adriano dos Santos Moreira
+ * @author Miguel Luna García
+ * @since Feb 27 2023
+ * @desc Deeply nested statements example.
+ */
+
+function deeplyNested(input1: number,
+                      input2: number) {
+  if (!Number.isNaN(input1)) {
+    if (!Number.isNaN(input2)) {
+      if (input1 > 0) {
+        if (input1 < Infinity) {
+          switch (input1) {
+            case 1:
+              if (input2 > 0) {
+                /** Code */
+              }
+          }
+        }
+      }
+    }
+  }
+}
